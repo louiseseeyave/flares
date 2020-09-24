25624c953af1b49c42e441058a95d374e6818b1b
Aswin Payyoor Vijayan <aswinpvijayan@gmail.com>
2020-09-24 16:48:38 +0100

    added script to calculate los metal density for star particles

---
+++ calc_Zlos.py
@@ -0,0 +1,205 @@
+import timeit, sys
+import numpy as np
+from mpi4py import MPI
+from numba import jit, njit, float64, int32, prange
+import h5py
+from functools import partial
+import schwimmbad
+from astropy.cosmology import Planck13 as cosmo
+from astropy import units as u
+
+import flares
+
+conv = (u.solMass/u.Mpc**2).to(u.solMass/u.pc**2)
+
+
+@njit(float64[:](float64[:,:], float64[:,:], float64[:], float64[:], float64[:], float64[:], int32), parallel=True, nogil=True)
+def get_Z_LOS(s_cood, g_cood, g_mass, g_Z, g_sml, lkernel, kbins):
+
+    """
+
+    Compute the los metal surface density (in Msun/Mpc^2) for star particles inside the galaxy taking
+    the z-axis as the los.
+    Args:
+        s_cood (3d array): stellar particle coordinates
+        g_cood (3d array): gas particle coordinates
+        g_mass (1d array): gas particle mass
+        g_Z (1d array): gas particle metallicity
+        g_sml (1d array): gas particle smoothing length
+
+    """
+    n = len(s_cood)
+    Z_los_SD = np.zeros(n)
+    #Fixing the observer direction as z-axis.
+    xdir, ydir, zdir = 0, 1, 2
+    for ii in prange(n):
+
+        thisspos = s_cood[ii]
+        ok = np.where(g_cood[:,zdir] > thisspos[zdir])[0]
+        thisgpos = g_cood[ok]
+        thisgsml = g_sml[ok]
+        thisgZ = g_Z[ok]
+        thisgmass = g_mass[ok]
+        x = thisgpos[:,xdir] - thisspos[xdir]
+        y = thisgpos[:,ydir] - thisspos[ydir]
+
+        b = np.sqrt(x*x + y*y)
+        boverh = b/thisgsml
+
+        ok = np.where(boverh <= 1.)[0]
+        kernel_vals = np.array([lkernel[int(kbins*ll)] for ll in boverh[ok]])
+
+        Z_los_SD[ii] = np.sum((thisgmass[ok]*thisgZ[ok]/(thisgsml[ok]*thisgsml[ok]))*kernel_vals) #in units of Msun/Mpc^2
+
+
+    return Z_los_SD
+
+
+def get_data(ii, tag, inp = 'FLARES'):
+
+    num = str(ii)
+    if inp == 'FLARES':
+        if len(num) == 1:
+            num =  '0'+num
+        filename = 'data2/FLARES_{}_sp_info.hdf5'.format(num)
+        sim_type = 'FLARES'
+
+
+    elif inp == 'REF' or inp == 'AGNdT9':
+        filename = F"EAGLE_{inp}_sp_info.hdf5"
+        sim_type = 'PERIODIC'
+
+    with h5py.File(filename, 'r') as hf:
+        S_len = np.array(hf[tag+'/Galaxy'].get('S_Length'), dtype = np.int64)
+        G_len = np.array(hf[tag+'/Galaxy'].get('G_Length'), dtype = np.int64)
+        S_coords = np.array(hf[tag+'/Particle'].get('S_Coordinates'), dtype = np.float64)
+        G_coords = np.array(hf[tag+'/Particle'].get('G_Coordinates'), dtype = np.float64)
+        G_mass = np.array(hf[tag+'/Particle'].get('G_Mass'), dtype = np.float64)*1e10
+        G_sml = np.array(hf[tag+'/Particle'].get('G_sml'), dtype = np.float64)
+        G_Z = np.array(hf[tag+'/Particle'].get('G_Z_smooth'), dtype = np.float64)
+
+
+    return S_coords, G_coords, G_mass, G_sml, G_Z, S_len, G_len
+
+
+def get_len(Length):
+
+    begin = np.zeros(len(Length), dtype = np.int64)
+    end = np.zeros(len(Length), dtype = np.int64)
+    begin[1:] = np.cumsum(Length)[:-1]
+    end = np.cumsum(Length)
+
+    return begin, end
+
+
+def get_ZLOS(jj, S_coords, G_coords, G_mass, G_Z, G_sml, sbegin, send, gbegin, gend, lkernel, kbins):
+
+    this_scoords = S_coords[sbegin[jj]:send[jj]]
+    this_gcoords = G_coords[gbegin[jj]:gend[jj]]
+
+    this_gmass = G_mass[gbegin[jj]:gend[jj]]
+    this_gZ = G_Z[gbegin[jj]:gend[jj]]
+    this_gsml = G_sml[gbegin[jj]:gend[jj]]
+
+
+    Z_los_SD = get_Z_LOS(this_scoords, this_gcoords, this_gmass, this_gZ, this_gsml, lkernel, kbins)*conv
+
+    return Z_los_SD
+
+
+if __name__ == "__main__":
+
+
+    ii, tag, inp = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    ## MPI parameters
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    #sph kernel approximations
+    kinp = np.load('./data/kernel_sph-anarchy.npz', allow_pickle=True)
+    lkernel = kinp['kernel']
+    header = kinp['header']
+    kbins = header.item()['bins']
+
+
+    #For galaxies in region `num` and snap = tag
+    num = str(ii)
+    if len(num) == 1:
+        num = '0'+num
+    S_coords, G_coords, G_mass, G_sml, G_Z, S_len, G_len = get_data(num, tag, inp = inp)
+    z = float(tag[5:].replace('p','.'))
+    S_coords=S_coords.T/(1+z)
+    G_coords=G_coords.T/(1+z)
+
+    total = np.arange(0, len(S_len))
+
+    comm.Barrier()
+
+    part = int(len(S_len)/size)
+    if rank!=size-1:
+        thisok = total[rank*part:(rank+1)*part]
+    else:
+        thisok = total[rank*part:]
+
+    sbegin, send = get_len(S_len)
+    gbegin, gend = get_len(G_len)
+
+    S_coords = S_coords[sbegin[thisok[0]]:send[thisok[-1]]]
+
+    G_coords = G_coords[gbegin[thisok[0]]:gend[thisok[-1]]]
+    G_mass = G_mass[gbegin[thisok[0]]:gend[thisok[-1]]]
+    G_Z = G_Z[gbegin[thisok[0]]:gend[thisok[-1]]]
+    G_sml = G_sml[gbegin[thisok[0]]:gend[thisok[-1]]]
+
+
+    if rank!=size-1:
+        S_len = S_len[thisok[0]:thisok[-1]]
+        G_len = G_len[thisok[0]:thisok[-1]]
+    else:
+        S_len = S_len[thisok[0]:]
+        G_len = G_len[thisok[0]:]
+
+    sbegin, send = get_len(S_len)
+    gbegin, gend = get_len(G_len)
+
+    start = timeit.default_timer()
+    calc_Zlos = partial(get_ZLOS, S_coords=S_coords, G_coords=G_coords, G_mass=G_mass, G_Z=G_Z, G_sml=G_sml, sbegin=sbegin, send=send, gbegin=gbegin, gend=gend, lkernel=lkernel, kbins=kbins)
+    pool = schwimmbad.MultiPool(processes=4)
+    tZlos = np.concatenate(np.array(list(pool.map(calc_Zlos, np.arange(0,len(sbegin))))))
+    pool.close()
+    stop = timeit.default_timer()
+    print (F"Took {np.round(stop - start, 6)} seconds for rank = {rank}")
+
+    comm.Barrier()
+
+    if rank == 0:
+
+        print ("Gathering data from different processes")
+
+    Zlos = comm.gather(tZlos, root=0)
+
+    if rank == 0:
+
+        print ("Gathering completed")
+
+        Zlos = np.concatenate(np.array(Zlos))
+        if inp == 'FLARES':
+            if len(num) == 1:
+                num =  '0'+num
+            filename = 'data2/FLARES_{}_sp_info.hdf5'.format(num)
+            sim_type = 'FLARES'
+
+
+        elif inp == 'REF' or inp == 'AGNdT9':
+            filename = F"EAGLE_{inp}_sp_info.hdf5"
+            sim_type = 'PERIODIC'
+
+        print(F"Wrting out line-of-sight metal density to {filename}")
+        print (Zlos)
+
+        fl = flares.flares(fname = filename,sim_type = sim_type)
+
+        fl.create_dataset(Zlos, 'S_los', '{}/Particle'.format(tag),
+            desc = 'Star particle line-of-sight metal column density along the z-axis', unit = 'Msun/pc^2', overwrite=True)
---
+++ get_Zlos.cosma.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH --ntasks 14
+#SBATCH -A dp004
+#SBATCH -p cosma7
+#SBATCH --job-name=get_FLARES
+####SBATCH --array=0-30%10
+###SBATCH --array=0-3%1
+#SBATCH -t 0-00:08
+###SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=14
+#SBATCH -o logs/std_output.%J
+#SBATCH -e logs/std_error.%J
+
+module purge
+module load gnu_comp/7.3.0 openmpi/3.0.1 hdf5/1.10.3 python/3.6.5
+
+source ./venv_fl/bin/activate
+
+### For FLARES galaxies, change ntasks as required
+# array=(011_z004p770 010_z005p000 009_z006p000) #008_z007p000 007_z008p000 006_z009p000 005_z010p000 004_z011p000 003_z012p000 002_z013p000 001_z014p000 000_z015p000)
+
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[0]} FLARES
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[1]} FLARES
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[2]} FLARES
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[3]} FLARES
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[4]} FLARES
+# mpiexec -n 16 -m mpi4py python3 calc_Zlos.py $SLURM_ARRAY_TASK_ID ${array[5]} FLARES
+
+
+### For PERIODIC boxes: REF and AGNdT9, change ntasks and time as required
+# array=(002_z009p993 003_z008p988 004_z008p075 005_z007p050 006_z005p971 008_z005p037)
+# array=(006_z005p971 008_z005p037 009_z004p485 z003p984)
+
+mpiexec -n 14 python3 -m mpi4py calc_Zlos.py 0 002_z009p993 REF
+
+# mpiexec -n 64 python3 -m mpi4py download_methods.py $SLURM_ARRAY_TASK_ID ${array[$SLURM_ARRAY_TASK_ID]} REF req_arrs.txt
+
+echo "Job done, info follows..."
+sacct -j $SLURM_JOBID --format=JobID,JobName,Partition,MaxRSS,Elapsed,ExitCode
+exit
