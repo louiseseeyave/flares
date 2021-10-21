86bd9428ec94aded47c3f2f3295828f3f4cc65dd
polyphant1 <chrisl1991@hotmail.co.uk>
2021-10-21 11:53:23 +0100

    added test script

---
+++ test_master.py
@@ -0,0 +1,46 @@
+
+
+import h5py
+
+
+f = '/cosma7/data/dp004/dc-love2/codes/flares/data/flares.hdf5'
+
+
+# with h5py.File(f, mode='r') as hf:
+#
+#     sims = hf.keys()
+#     tags = hf[list(sims)[0]].keys()
+#
+#     for sim in sims:
+#         for tag in tags:
+#             Datasetnames=hf[sim][tag].keys()
+#             print(sim, tag, Datasetnames)
+
+
+with h5py.File(f, mode='r') as hf:
+
+    sims = hf.keys()
+    tags = hf[list(sims)[0]].keys()
+
+    for sim in sims:
+        for tag in tags:
+
+            try:
+                Ngal_Mstar = len(hf[sim][tag]['Galaxy/Mstar'][:])
+            except:
+                Ngal_Mstar = 'FAILED'
+
+            try:
+                Ngal_Lum = len(hf[sim][tag]['Galaxy/BPASS_2.2.1/Chabrier300/Luminosity/DustModelI/FUV'][:])
+            except:
+                Ngal_Lum = 'FAILED'
+
+            if Ngal_Lum == 'FAILED' or Ngal_Mstar == 'FAILED':
+                print(sim, tag, Ngal_Mstar, Ngal_Lum)
+            elif Ngal_Mstar-Ngal_Lum != 0:
+                print(sim, tag, Ngal_Mstar, Ngal_Lum)
+
+
+
+            # else:
+            #     print(sim, tag, Ngal_Mstar, Ngal_Lum, Ngal_Mstar-Ngal_Lum)
